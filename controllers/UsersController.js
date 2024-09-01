0e8aade9a2f855da41d1e1b4f65bc476cfa84406
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-09-01 21:19:58 +0100

    handled break in password validation

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -8,9 +8,11 @@ export default class UsersController {
     const { email, password } = req.body;
     if (!email) {
       res.status(400).json({ error: 'Missing email' });
+      return;
     }
     if (!password) {
       res.status(400).send({ error: 'Missing password' });
+      return;
     }
 
     const users = dbClient.db.collection('users');
@@ -25,19 +27,22 @@ export default class UsersController {
     const userId = newUser.insertedId;
     if(newUser){
     res.status(201).send({ id: userId, email });
+    return;
     }
   }
 
   static async getMe(req, res) {
-    const xtoken = req.headers['x-token'];
-    const key = `auth_${xtoken}`;
+    const xToken = req.headers['x-token'];
+    const key = `auth_${xToken}`;
     const userId = await redisClient.get(key);
     const users = dbClient.db.collection('users');
     const user = await users.findOne({ _id: ObjectId(userId) });
     if (user) {
       res.json({ id: user._id, email: user.email});
+      return;
     } else {
       res.status(401).json({ error: 'Unauthorised' });
+      return;
     }
   }
 }
