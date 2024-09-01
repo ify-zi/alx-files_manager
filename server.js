2ad20e4a6dd2a6d99f33e23c9b1fe76202e62218
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-09-01 23:31:44 +0100

    reslve bugs trial 6

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import route from './routes/index';
-
-const express = require('express');
+import express from 'express';
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
+
 app.use('/', route);
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log('Server is running on http://127.0.0.1:5000');
 });
+
+export default app;
